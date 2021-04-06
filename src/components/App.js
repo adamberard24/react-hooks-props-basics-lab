7d39c5389aa5f9f7ea6341630ae4bde651ba1a90
Adam Schwartz <adamberard24@gmail.com>
2021-04-05 23:03:51 -0400

    completed assignment with help

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,18 @@ console.log(user);
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+
+     <NavBar />
+
+      <Home 
+      username = {user.name}
+      city = {user.city}
+      color = {user.color}
+      />
+      <About 
+      bio = {user.bio}
+      github = {user.links.github}
+      linkedin = {user.links.linkedin}/>
     </div>
   );
 }
